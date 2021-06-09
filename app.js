776b03d46a04ef7963eedf410a10d264da8a6c85
jakeschultz89 <ejacobschultz@gmail.com>
2021-06-09 16:17:46 -0400

    fixed astros API route

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const passport = require('passport');
 const PORT = process.env.PORT || 8000;
 
 // API
-const astros = require('astros');
+const astros = require('./api/astros');
 const crews = require('./api/crews')
 const dragons = require('./api/dragons');
 const rockets = require('./api/rockets');
