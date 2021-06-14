5dd09869b57a73a0e277973453dae8f3f591710a
jakeschultz89 <ejacobschultz@gmail.com>
2021-06-14 12:20:11 -0400

    Merge branch 'main' of https://github.com/jakeschultz89/Project-3-Backend

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const passport = require('passport');
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 // app.listen(app.get("port"), () => {
 //     console.log(`✅ PORT: ${app.get("port")} 🌟`);
